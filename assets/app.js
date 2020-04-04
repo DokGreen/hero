8bacbd4be675aaffa94a0eb144b993ae4468651c
Reece <rfarnworth92@gmail.com>
2020-04-04 12:41:06 +0100

    funtioning

--- assets/app.js
+++ assets/app.js
@@ -4,35 +4,47 @@ $(document).ready(() => {
 });
 */
 
-$()
-
-$(document).on('keydown', (e) => {
-    if(e.key === 'Enter') {
+$('#hBtn').on('click', (e) => {
         //console.log('clicked');
+        //generate random number
         //set var
-        let name = $('#name').val();
-        redPhone(name);
-    }
+        let id = Math.floor((Math.random() * 731) + 1);
+        redPhone(id);
 });
 
-function redPhone(name) {
-    axios.get('https://superheroapi.com/api/10219050137697538/'+name)
+function redPhone(id) {
+    axios.get('https://superheroapi.com/api/10219050137697538/'+id)
     .then((r) => {
-        console.log(r);
+        $('.char').html = ' ';
+
+        //console.log(r);
+
         let hName = r.data.name;
-        let powers = r.data.powerstats;
+        let rName = r.data.biography['full-name']; //---ISSUE
+        let race = r.data.appearance.race;
+        let base = r.data.work.base;
+        let family = r.data.connections.relatives;
         let img = r.data.image.url;
+        let fAppearance = r.data.biography['first-appearance']; //---ISSUE
+        let groups = r.data.connections['group-affiliation']; //---ISSUE
 
         let output = '';
 
         output += 
         `
         <h3>${hName}</h3>
+        <h5>(${rName})</h5>
+        <h5>(${race})</h5>
         <img src="${img}"/>
-        <p>${powers}</p>
+        <p>${fAppearance}</p>
+        <hr/>
+        <p>Base of opperations: ${base}</p>
+        <p>Family: ${family}</p>
+        <p>Member of: ${groups}</p> 
+        <p>Finish with powers and abilities</p>
         `
 
-        $('body').append(output);
+        $('.char').append(output);
     })
     .catch((err) => {
         console.log(err);
